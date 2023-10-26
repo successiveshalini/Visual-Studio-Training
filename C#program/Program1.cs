f1ab13d84554a1f88970606c099e8788fb5d660f
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-10-26 13:15:15 +0530

    some changes in file

---
+++ C#program/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a string to Check Palindrome : ");
+        string name = Console.ReadLine();
+        string reverse = string.Empty;
+        foreach (char c in name)
+        {
+            reverse = c + reverse;
+        }
+        if (name.Equals(reverse, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"{name} is Palindrome");
+        }
+        else
+        {
+            Console.WriteLine($"{name} is not Palindrome");
+        }
+        Console.ReadLine();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
