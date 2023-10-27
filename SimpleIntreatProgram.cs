16cfa99b245384750eebc34282088fc83d654f48
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-10-27 13:41:55 +0530

    simple intrest cal

---
+++ SimpleIntreatProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInterestcalculater
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double si, p, t, r;
+            Console.WriteLine("Enter the principle");
+            p = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the time");
+            t = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the rate");
+            r = double.Parse(Console.ReadLine());
+
+            si = (p * t * r) / 100;
+            Console.WriteLine(si);
+            Console.ReadKey();  
+            //hhhhhhhhhhhhhhhhhhhhhhhhhhh
+        }
+    }
+}
